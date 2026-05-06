8abc3dabd5fc6f6bc25e05382cb841f72ca8c2ca
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-06 22:09:02 +0600

    problem 3 solved

--- solutions.ts
+++ solutions.ts
@@ -1,6 +1,6 @@
 // Problem 1
 
-const filterEvenNumbers = (input: number[]) =>{
+const filterEvenNumbers = (input: number[]): number[] =>{
     const filter = input.filter((even) => even % 2 === 0)
     return filter
 } 
@@ -8,9 +8,25 @@ console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]))
 
 // Problem 2
 
-const reverseString = (input: string) =>{
+const reverseString = (input: string): string =>{
     const reverse = input.split("").reverse().join("")
     return reverse
 
 }
 console.log(reverseString("typescript"))
+
+// Problem 3
+
+type Input = string | number
+
+const checkType = (input: Input) =>{
+    if(typeof input === 'string'){
+        return "String"
+    }else{
+        return "Number"
+    }
+
+}
+
+console.log(checkType("Hello"))
+console.log(checkType(42))

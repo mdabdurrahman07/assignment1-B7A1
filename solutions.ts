acb913e7d5c634ba0456bc81ab4d053a4b5f9662
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-06 23:34:25 +0600

    problem 6 solved

--- solutions.ts
+++ solutions.ts
@@ -60,3 +60,27 @@ console.log(
     publishedYear: 2024,
   }),
 );
+
+// Problem 6
+
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+  getDetails() {
+    return `"Name:${this.name}, Age:${this.age}, Grade:${this.grade}"`
+  }
+}
+const student = new Student("Alice", 20, "A");
+console.log(student.getDetails())

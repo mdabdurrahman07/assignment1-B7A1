f21e7c87530c77c2b5d0375e62e82ee7f04cc253
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-06 22:59:13 +0600

    problem 3 & 4 solved and added  in the return using temple literal

--- solutions.ts
+++ solutions.ts
@@ -1,32 +1,62 @@
 // Problem 1
 
-const filterEvenNumbers = (input: number[]): number[] =>{
-    const filter = input.filter((even) => even % 2 === 0)
-    return filter
-} 
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]))
+const filterEvenNumbers = (input: number[]): number[] => {
+  const filter = input.filter((even) => even % 2 === 0);
+  return filter;
+};
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
 // Problem 2
 
-const reverseString = (input: string): string =>{
-    const reverse = input.split("").reverse().join("")
-    return reverse
-
-}
-console.log(reverseString("typescript"))
+const reverseString = (input: string): string => {
+  const reverse = input.split("").reverse().join("");
+  return `"${reverse}"`;
+};
+console.log(reverseString("typescript"));
 
 // Problem 3
 
-type Input = string | number
+type Input = string | number;
 
-const checkType = (input: Input) =>{
-    if(typeof input === 'string'){
-        return "String"
-    }else{
-        return "Number"
-    }
+const checkType = (input: Input) => {
+  if (typeof input === "string") {
+    return `"String"`;
+  } else {
+    return `"Number"`;
+  }
+};
 
+console.log(checkType("Hello"));
+console.log(checkType(42));
+
+// Problem 4
+
+const getProperty = <T>(obj: T, key: keyof T) => {
+  return `"${obj[key]}"`;
+};
+const user = { id: 1, name: "John Doe", age: 21 };
+const result = getProperty(user, "name");
+console.log(result);
+
+// Problem 5
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
 }
 
-console.log(checkType("Hello"))
-console.log(checkType(42))
+const toggleReadStatus = (book: Book) => {
+  return {
+    ...book,
+    isRead: true,
+  };
+};
+
+console.log(
+  toggleReadStatus({
+    title: "TypeScript Guide",
+    author: "Jane Doe",
+    publishedYear: 2024,
+  }),
+);
